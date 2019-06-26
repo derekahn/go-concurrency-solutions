6cdf81a344d1dd3c270739987255487e83d6e4a6
Derek Ahn <git.derek@gmail.com>
2019-06-26 14:24:06 -0700

    💡0-limit-crawler; SOLVED
    - [Hint](https://github.com/golang/go/wiki/RateLimiting) was nice!

--- 0-limit-crawler/main.go
+++ 0-limit-crawler/main.go
@@ -17,13 +17,14 @@ import (
 
 // Crawl uses `fetcher` from the `mockfetcher.go` file to imitate a
 // real crawler. It crawls until the maximum depth has reached.
-func Crawl(url string, depth int, wg *sync.WaitGroup) {
+func Crawl(url string, depth int, wg *sync.WaitGroup, throttle <-chan time.Time) {
 	defer wg.Done()
 
 	if depth <= 0 {
 		return
 	}
 
+	<-throttle
 	body, urls, err := fetcher.Fetch(url)
 	if err != nil {
 		fmt.Println(err)
@@ -32,15 +33,12 @@ func Crawl(url string, depth int, wg *sync.WaitGroup) {
 
 	fmt.Printf("found: %s %q\n", url, body)
 
-	rate := time.Second * 2
-	throttle := time.Tick(rate)
-
 	wg.Add(len(urls))
+
 	for _, u := range urls {
-		<-throttle
 		// Do not remove the `go` keyword, as Crawl() must be
 		// called concurrently
-		go Crawl(u, depth-1, wg)
+		go Crawl(u, depth-1, wg, throttle)
 	}
 	return
 }
@@ -49,6 +47,8 @@ func main() {
 	var wg sync.WaitGroup
 
 	wg.Add(1)
-	Crawl("http://golang.org/", 4, &wg)
+
+	throttle := time.Tick(time.Second)
+	Crawl("http://golang.org/", 4, &wg, throttle)
 	wg.Wait()
 }
